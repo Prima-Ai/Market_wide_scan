5c2998dbd361a0af191d58eacc5ca1d2ddc96482
Prime <prathmeshlandge044@outlook.com>
2024-07-05 12:09:02 +0530

    code till only market scan without removing null values

--- mws.py
+++ mws.py
@@ -10,22 +10,26 @@ class market_wide_scan:
             data = yf.download(f"{ticker_name}.NS")
             data.to_csv(f"C:/MY_PROJECTS/market_wide_scan/data/{ticker_name}.csv")
     
-    def oneweek_scan(self,path):
-        data = pd.read_csv(path, parse_dates=['Date'])
+    def get_paras(path,NOD,MA,column_name):
+        data = pd.read_csv(path)
+        data['Date'] = pd.to_datetime(data['Date'])
         data.set_index('Date',inplace=True)
-        print("the lenght of data is",len(data))
-        
-        # calculate weekly prices
-        weekly_prices = data['Close'].resample('W').last()
+        print("length of data is",len(data))
+        weekly_prices = data['Close'].resample(NOD).last()
         print("lenght of weekly prices",len(weekly_prices))
-        data['weekly'] = weekly_prices.reindex(data.index, method='ffill')
-        
-        # calculate 60ma for weekly prices
-        weekly_prices_60ma = weekly_prices.rolling(window=60).mean()
-        data['60_weekly_ma'] = weekly_prices_60ma.reindex(data.index, method='ffill')
-        data_till60ma = data.to_csv("C:/MY_PROJECTS/market_wide_scan/op_files/ADANIENT.csv")
-        return "script ran sucessfully"
-
+        data[column_name] = weekly_prices.reindex(data.index,method='ffill')
+        # calculate 60ma for weekly data
+        weekly_prices_60ma = weekly_prices.rolling(window=MA).mean()
+        data['60ma'] = weekly_prices_60ma.reindex(data.index, method='ffill')
+        fall = []
+        for i in range(len(data)-1):
+            fall_of_extent = ((data['60ma'][i+1] - data['60ma'][i])/data['60ma'][i])*100
+            fall.append(fall_of_extent)
+        fall.append(0.0)
+        print("lenght of fall list",len(fall))
+        data['extent_of_fall'] = fall
+        data = data.to_csv("C:/MY_PROJECTS/market_wide_scan/op_files/Daily.csv")
+        return "done all process"
 
 path = "C:/MY_PROJECTS/market_wide_scan/data/ADANIENT.csv"
 csv = "C:/MY_PROJECTS/market_wide_scan/n200.csv"
